074348ca6e13a7af14cfb53feb698567a40aa57e
Parimala-429 <parimala.jpj@gmail.com>
2023-05-24 13:52:55 +0100

    Changes to Pom.xml

--- src/main/java/jjgroupid/base/BaseClass.java
+++ src/main/java/jjgroupid/base/BaseClass.java
@@ -1,4 +1,5 @@
-package jjgroupid;
+package jjgroupid.base;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 //import org.openqa.selenium.chrome.ChromeDriver;
@@ -21,3 +22,4 @@ static WebDriver driver;
 //return (driver);
     }
 }
+ 
